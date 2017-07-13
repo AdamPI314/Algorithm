8cd98d34dc65d101634c324bf306c89f4a6d98d7
AdamPI <shirong.bai@colorado.edu>
2017-07-12 20:00:55 -0600

    update build.bat on Windows.

--- src/cppTemplate/build.bat
+++ src/cppTemplate/build.bat
@@ -1,5 +1,6 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64     
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64     
 set compilerflags=/Od /Zi /EHsc
 set linkerflags=/OUT:solution.exe
 set source=../../include/fileIO/fileIO.cpp ../../include/dataStructure/dataStructure.cpp ../../include/misc/misc.cpp
