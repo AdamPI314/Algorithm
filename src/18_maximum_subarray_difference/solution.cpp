79be51e73f1e796e31937d7b337fcb99308453dc
AdamPI <shirong.bai@colorado.edu>
2017-07-12 22:49:58 -0600

    18 Maximum Subarray Difference.

--- src/18_maximum_subarray_difference/solution.cpp
+++ src/18_maximum_subarray_difference/solution.cpp
@@ -18,45 +18,75 @@
 
 using namespace std;
 
-class Solution {
-public:
-    /**
+class Solution
+{
+  public:
+	/**
      * @param nums: A list of integers
      * @return: An integer indicate the value of maximum difference between two
      *          Subarrays
      */
-    int maxDiffSubArrays(vector<int> nums) {
-        // write your code here
+	int maxDiffSubArrays(vector<int> nums)
+	{
+		// write your code here
+		// key point is how to find minimum subarray
 		int n = nums.size();
-		if (n == 0)
+		if (n <= 1)
 			return 0;
-		if (n == 1)
-			return nums[0];
 
-		int result;
-		vector<vector<int> > sum(n, vector<int>(n, 0));
+		vector<int> leftMin(n), leftMax(n), rightMin(n), rightMax(n);
+		// negative copy
+		vector<int> copy(nums.begin(), nums.end());
+		for (auto &x : copy)
+			x *= -1;
 
-		// subarray includes at least one element
+		int sum = 0, minSum = 0, maxSum = INT_MIN;
+		// forward, get max subarray
 		for (int i = 0; i < n; ++i)
-			sum[0][i] = nums[i];
-
-		for (int i = 1; i < n; ++i) {
-			for (int j = i; j < n; ++j ) {
-								
-			}
+		{
+			sum += nums[i];
+			maxSum = max(maxSum, sum - minSum);
+			leftMax[i] = maxSum;
+			minSum = min(minSum, sum);
 		}
-
-
-
-		return result;
-    }
+		// backward, get max subarray
+		sum = 0; minSum = 0; maxSum = INT_MIN;
+		for (int i = n-1; i >= 0; --i) {
+			sum += nums[i];
+			maxSum = max(maxSum, sum - minSum);
+			rightMax[i] = maxSum;
+			minSum = min(minSum, sum);
+		}
+		// forward, get min subarray
+		sum = 0; minSum = 0; maxSum = INT_MIN;
+		for (int i = 0; i < n; ++i)
+		{
+			sum += copy[i];
+			maxSum = max(maxSum, sum - minSum);
+			leftMin[i] = -1*maxSum;
+			minSum = min(minSum, sum);
+		}
+		// backward, get min subarray
+		sum = 0; minSum = 0; maxSum = INT_MIN;
+		for (int i = n-1; i >= 0; --i) {
+			sum += copy[i];
+			maxSum = max(maxSum, sum - minSum);
+			rightMin[i] = -1*maxSum;
+			minSum = min(minSum, sum);
+		}
+		
+		int diff = 0;
+		for (int i = 0; i < n-1; ++i) {
+			diff = max(diff, max(abs(leftMax[i]-rightMin[i+1]), abs(rightMax[i+1]-leftMin[i])));
+		}
+		return diff;
+	}
 };
-
 
 int main(int argc, char **argv)
 {
 	// initialization, data preparation
-	vector<int> nums = {1,2,-3,1};
+	vector<int> nums = {-5, -4};
 
 	// my solution
 	Solution sln;
