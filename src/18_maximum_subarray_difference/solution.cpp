2fe1899a226842627e342e12ca43085ac18bb7a3
AdamPI <shirong.bai@colorado.edu>
2017-07-12 20:05:54 -0600

    18 Maximum Subarray Difference alpha.

---
+++ src/18_maximum_subarray_difference/solution.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <numeric>
+#include <functional>
+
+#include "../../include/fileIO/fileIO.h"
+#include "../../include/dataStructure/dataStructure.h"
+#include "../../include/misc/misc.h"
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param nums: A list of integers
+     * @return: An integer indicate the value of maximum difference between two
+     *          Subarrays
+     */
+    int maxDiffSubArrays(vector<int> nums) {
+        // write your code here
+		int result;
+
+		return result;
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+	// initialization, data preparation
+	vector<int> nums = {1,2,-3,1};
+
+	// my solution
+	Solution sln;
+	auto result = sln.maxDiffSubArrays(nums);
+
+	// correct answer
+
+	return EXIT_SUCCESS;
+}
