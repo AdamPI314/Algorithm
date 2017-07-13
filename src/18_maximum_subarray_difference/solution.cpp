2a8d673bb6064a235c9b2f130beecc1fc68c73c1
AdamPI <shirong.bai@colorado.edu>
2017-07-12 20:58:29 -0600

    18 Maximum Subarray Difference beta.

--- src/18_maximum_subarray_difference/solution.cpp
+++ src/18_maximum_subarray_difference/solution.cpp
@@ -27,7 +27,26 @@ public:
      */
     int maxDiffSubArrays(vector<int> nums) {
         // write your code here
+		int n = nums.size();
+		if (n == 0)
+			return 0;
+		if (n == 1)
+			return nums[0];
+
 		int result;
+		vector<vector<int> > sum(n, vector<int>(n, 0));
+
+		// subarray includes at least one element
+		for (int i = 0; i < n; ++i)
+			sum[0][i] = nums[i];
+
+		for (int i = 1; i < n; ++i) {
+			for (int j = i; j < n; ++j ) {
+								
+			}
+		}
+
+
 
 		return result;
     }
