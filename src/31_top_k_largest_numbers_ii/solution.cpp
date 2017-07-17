c060ae059fdc4ee5abcd4bb9164945d0cfcdcb4f
AdamPI <shirong.bai@colorado.edu>
2017-07-17 09:50:58 -0600

    31 Top k Largest Numbers II.

---
+++ src/31_top_k_largest_numbers_ii/solution.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <forward_list>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <numeric>
+#include <functional>
+
+#include "../../include/fileIO/fileIO.h"
+#include "../../include/dataStructure/dataStructure.h"
+#include "../../include/misc/misc.h"
+
+using namespace std;
+
+class Solution {
+public:
+	int k = 0;
+	set<int> hash();
+
+public:
+    Solution(int k) {
+        // initialize your data structure here.
+		this.k = k;
+    }
+
+    void add(int num) {
+        // Write your code here
+		hash.insert(num);
+		if (hash.size() > k)
+
+    }
+
+    vector<int> topk() {
+        // Write your code here
+    }
+};
+
+
+int main(int argc, char **argv)
+{
+	// initialization, data preparation
+
+	// my solution
+	Solution sln;
+
+	// correct answer
+
+	return EXIT_SUCCESS;
+}
