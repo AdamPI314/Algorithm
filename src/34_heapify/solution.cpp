bc84216c86c9df2c260bffc5d147223bf59b6d69
AdamPI <shirong.bai@colorado.edu>
2017-07-17 14:43:34 -0600

    34 Heapify.

---
+++ src/34_heapify/solution.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <forward_list>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <numeric>
+#include <functional>
+#include <iterator>
+
+#include "../../include/fileIO/fileIO.h"
+#include "../../include/dataStructure/dataStructure.h"
+#include "../../include/misc/misc.h"
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param A: Given an integer array
+     * @return: void
+     */
+    void heapify(vector<int> &A) {
+        // write your code here
+		
+    }
+};
+
+int main(int argc, char **argv)
+{
+	// initialization, data preparation
+	vector<int> A = {3,2,1,4,5};
+	// my solution
+	Solution sln;
+	sln.heapify(A);
+
+	// correct answer
+
+	return EXIT_SUCCESS;
+}
