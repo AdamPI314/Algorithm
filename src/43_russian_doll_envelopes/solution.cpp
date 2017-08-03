2bfb77f802a8ab6a6e89dd71b2e30c09e22c935a
AdamPI <shirong.bai@colorado.edu>
2017-08-02 21:46:04 -0600

    43 Russian Doll Envelopes.

--- src/43_russian_doll_envelopes/solution.cpp
+++ src/43_russian_doll_envelopes/solution.cpp
@@ -23,40 +23,46 @@
 
 using namespace std;
 
-bool cmp(const pair<int,int>&x, const pair<int, int>&y) {
-  return x.first != y.first ? x.first < y.first : x.second > y.second;
+bool cmp(const pair<int, int> &x, const pair<int, int> &y)
+{
+    return x.first != y.first ? x.first < y.first : x.second > y.second;
 }
 
-class Solution {
-public:
+class Solution
+{
+  public:
     /**
      * @param envelopes a number of envelopes with widths and heights
      * @return the maximum number of envelopes
      */
-    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+    int maxEnvelopes(vector<pair<int, int>> &envelopes)
+    {
         // Write your code here
         int n = envelopes.size();
-        if (n == 0) {
+        if (n == 0)
+        {
             return 0;
         }
-    
-        sort(envelopes.begin(), envelopes.end(), cmp);
-		// dp save (max - 1) up to current point, including current point
-        vector<int> dp(n), height(n+1, INT_MAX);
-        for (int i = 0; i < n; i++) {
-			// replace the first value that is greater than envelopes[i].second, height is ordered
-			// if envelopes[i].second is greater than all previous value, append it to the end
 
-			// As a result, height store all the y-axis value of points to which x-axis < current-x-axis,
-			// y-axis < current-y-axis
-            int k = lower_bound(height.begin(), height.end(), envelopes[i].second) - height.begin() ;
+        sort(envelopes.begin(), envelopes.end(), cmp);
+        // dp save (max - 1) up to current point, including current point
+        vector<int> dp(n), height(n + 1, INT_MAX);
+        for (int i = 0; i < n; i++)
+        {
+            // replace the first value that is greater than envelopes[i].second, height is ordered
+            // if envelopes[i].second is greater than all previous value, append it to the end
+
+            // As a result, height store all the y-axis value of points to which x-axis < current-x-axis,
+            // y-axis < current-y-axis
+            int k = lower_bound(height.begin(), height.end(), envelopes[i].second) - height.begin();
             height[k] = envelopes[i].second;
-            
-			dp[i] = k;
+
+            dp[i] = k;
         }
-    
+
         int ans = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             ans = max(ans, dp[i]);
         }
         return ans + 1;
@@ -65,13 +71,13 @@ public:
 
 int main(int argc, char **argv)
 {
-	// initialization, data preparation
-	vector<pair<int, int>> envelopes = {{15, 8}, {2, 20}, {2, 14}, {4, 17}, {8, 19}, {8, 9}, {5, 7}, {11, 19}, {8, 11}, {13, 11}, {2, 13}, {11, 19}, {8, 11}, {13, 11}, {2, 13}, {11, 19}, {16, 1}, {18, 13}, {14, 17}, {18, 19}};
-	// my solution
-	Solution sln;
-	auto result = sln.maxEnvelopes(envelopes);
+    // initialization, data preparation
+    vector<pair<int, int>> envelopes = {{15, 8}, {2, 20}, {2, 14}, {4, 17}, {8, 19}, {8, 9}, {5, 7}, {11, 19}, {8, 11}, {13, 11}, {2, 13}, {11, 19}, {8, 11}, {13, 11}, {2, 13}, {11, 19}, {16, 1}, {18, 13}, {14, 17}, {18, 19}};
+    // my solution
+    Solution sln;
+    auto result = sln.maxEnvelopes(envelopes);
 
-	// correct answer
+    // correct answer
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
