79bdd58941f8bcc0d1eb50a9454edf32ee7ec319
AdamPI314 <shirong.bai@colorado.edu>
2017-08-15 20:04:09 -0600

    46 Drop Eggs II.

--- src/45_drop_eggs_ii/solution.cpp
+++ src/45_drop_eggs_ii/solution.cpp
@@ -30,31 +30,49 @@ public:
   /**
      * @param m the number of eggs
      * @param n the umber of floors
-     * @return the number of drops in the worst case
+     * @return the number of drops in the worst case supposing the best strategy
      */
-  int dropEggs2(int m, int n)
+  // with M eggs and k floors, supposing using the best strategy, in the worst case, the number of trials/drops is
+  // find a smallest d, such that
+  // sum_{i=1}^{M}{C(d,i)} >= k holds
+
+  // calculate the sum of binomials, verify if it is greater than x
+  // M eggs, d trials/drops
+  long long int sum_binomial(int M, int d, long long x)
   {
-    // Write your code here
-    vector<vector<int>> f(m+1, vector<int>(n+1, 0));
-    // boundary conditions
-    for (int i = 0; i <= m; ++i) {
-      f[i][0] = 0;
-    }
-    for (int i = 0; i <=n; ++i) {
-      f[1][i] = i;
-      f[0][i] = 0;
-    }
-    for (int i = 2; i <= m; ++i) {
-      for (int j = 1; j <= n; ++j) {
-        int min_drop = INT_MAX;
-        for (int k = 1; k <= j; ++k) {
-          min_drop = min(min_drop, 1 + max(f[i][j-k], f[i-1][k-1]));
-        }
-        f[i][j] = min_drop;
+    long long int result = 0;
+    double aux = 1;
+    for (int i = 1; i <= M; ++i)
+    {
+      aux /= (double)i;
+      aux *= (double)d - i + 1;
+      result += aux;
+
+      // since this a binary search job, if sum_binomial > k, there is no need to keep calculating
+      // because current scenario shall not be considered the best solution
+      if (result > x)
+      {
+        break;
       }
     }
+    return result;
+  }
 
-    return f[m][n];
+  int dropEggs2(int m, int n)
+  {
+    int begin = 0, end = n;
+    int mid = 0;
+
+    while (end - begin > 1)
+    {
+      // the reason don't use (begin+end)/2 is to avoid potential overflow
+      mid = begin + (end - begin) / 2;
+      if (sum_binomial(m, mid, n) < n)
+        begin = mid;
+      else
+        end = mid;
+    }
+    return begin + 1;
   }
 };
 
