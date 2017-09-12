5792cc22b919535efd2eae6aec5e817450e08fb1
AdamPI <shirong.bai@colorado.edu>
2017-09-11 20:11:34 -0600

    47 k Sum.

--- src/47_k_sum/solution.cpp
+++ src/47_k_sum/solution.cpp
@@ -36,15 +36,28 @@ public:
   int kSum(vector<int> A, int k, int target)
   {
     // wirte your code here
-    sort(A.begin(), A.end());
     int n = A.size();
-    vector<vector<int> > f(k+1, vector<int>(n+1, 0));
-    for (int i = 1; i <= k; ++i) {
-      for (int j = 1; j <= n; ++j) {
-        f[i][j] = 0;
-      }
+    vector<vector<vector<int>>> f(n + 1, vector<vector<int>>(k + 1, vector<int>(target + 1, 0)));
+    for (int i = 0; i < n + 1; i++)
+    {
+      f[i][0][0] = 1;
     }
-    return -1;
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= k && j <= i; j++)
+      {
+        for (int t = 1; t <= target; t++)
+        {
+          f[i][j][t] = 0;
+          if (t >= A[i - 1])
+          {
+            f[i][j][t] = f[i - 1][j - 1][t - A[i - 1]];
+          }
+          f[i][j][t] += f[i - 1][j][t];
+        }// for t
+      }// for j
+    } // for i
+    return f[n][k][target];
   }
 };
 
