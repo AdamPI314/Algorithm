4887f0ab567b95f8fe05b1ab9c066fac635f9911
AdamPI <shirong.bai@colorado.edu>
2018-03-21 21:28:51 -0600

    99 Remove Duplicates from Sorted List II.

--- src/99_remove_duplicates_from_sorted_list_ii/solution.cpp
+++ src/99_remove_duplicates_from_sorted_list_ii/solution.cpp
@@ -27,56 +27,29 @@ using namespace std;
 class Solution
 {
 public:
-  /**
-     * @param head: head is the head of the linked list
-     * @return: head of the linked list
-     */
   ListNode *deleteDuplicates(ListNode *head)
   {
-    // write your code here
-    if (head == nullptr)
-      return nullptr;
-
-    auto comp = [](ListNode *p1, ListNode *p2) { return p1->val < p2->val; };
-    unordered_map<ListNode *, int, decltype(comp)> myM;
-    vector<ListNode *> v;
-
-    auto curr = head;
-    while (curr != nullptr)
+    ListNode *root = NULL;
+    ListNode **ppNext = &root;
+    while (head)
     {
-      // check whether it appears before or not
-      myM[curr] += 1;
-      curr = curr->next;
-    }
-    curr = head;
-    ListNode *n_head = nullptr, *n_curr = nullptr;
-    while (curr != nullptr)
-    {
-      // it is a good one
-      if (myM.at(curr) == 1)
+      if (head->next == NULL || head->next->val != head->val)
       {
-        if (n_head == nullptr)
-        {
-          n_head = curr;
-          n_curr = n_head;
-        }
-        else
-        {
-          n_curr->next = curr;
-          n_curr = n_curr->next;
-        }
-        curr = curr->next;
-      } //if
+        *ppNext = head;
+        ppNext = &(head->next);
+        head = head->next;
+      }
       else
       {
-        // free the memory
-        auto x = curr;
-        curr = curr->next;
-        delete x;
-      } //else
+        int val = head->val;
+        do
+        {
+          head = head->next;
+        } while (head != NULL && head->val == val);
+      }
     }
-
-    return n_head;
+    *ppNext = NULL;
+    return root;
   }
 };
 
