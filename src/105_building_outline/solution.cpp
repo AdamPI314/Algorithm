14c0526c55b9b0d890b1f9038299d1a059166d26
AdamPI314 <shirong.bai@colorado.edu>
2018-03-27 16:00:21 -0600

    105 Building Outline.

--- src/105_building_outline/solution.cpp
+++ src/105_building_outline/solution.cpp
@@ -35,55 +35,71 @@ public:
   {
     // write your code here
     vector<vector<int>> ans;
+    if (buildings.size() == 0)
+      return ans;
 
-    // search for possible intervals, has form of (i, i+1)
-    // represented by i in a hashmap
-    // the second element represent the height of current inteval
-    // O(m*n)
-    // save both start and end index
-    map<int, int> myM;
+    // keep track of unsolved_sub intervals, guaranteed there will not be collision
+    set<int> un_visited;
+    map<int, pair<int, int>> myM;
 
+    // first, determine all critical points, O(n)
     for (auto x : buildings)
     {
-      // search, partition and assign value
-      if (myM.size() == 0)
-      {
-        myM[x[0]] = x[2];
-        myM[x[1]] = -1;
-        continue;
-      }
-      // else
-      // use binary search, find the range could be affected by this new building
-      auto iter_l = myM.lower_bound(x[0]);
-      auto iter_u = myM.upper_bound(x[1]);
-      // update
-      if (iter_l->first < x[0])
-      {
-        // a new building, no overlap
-        myM[x[0]] = x[2];
-        myM[x[1]] = -1;
-        continue;
-      }
+      if (myM.find(x[0]) == myM.end())
+        myM.emplace(x[0], make_pair(-1, -1));
+      if (myM.find(x[1]) == myM.end())
+        myM.emplace(x[1], make_pair(-1, -1));
+    }
+    // determine sub-intervals
+    auto p1 = myM.rbegin();
+    auto p2 = myM.rbegin();
+    ++p1; // p1 < p2
+    for (; p1 != myM.rend(); ++p1, ++p2)
+    {
+      p1->second.first = p2->first;
+      un_visited.emplace(p1->first);
+    }
+    myM.erase(prev(myM.end()));
 
+    // sort buildings by height, O(nlog(n))
+    auto comp = [](const vector<int> &v1, const vector<int> &v2) { return v1.back() > v2.back(); };
+    sort(buildings.begin(), buildings.end(), comp);
+
+    // from the largest height to the smallest height
+    for (auto x : buildings)
+    {
+      if (un_visited.size() <= 0)
+        break;
+      // search for iterator, guaranteed can find it, search un_visited instead
+      auto l_p = un_visited.lower_bound(x[0]);
+      auto h_p = un_visited.upper_bound(x[1] - 1); // can not be x[1]
+      auto l_p2 = l_p;
+      auto h_p2 = h_p;
+      while (l_p != h_p)
+      {
+        auto value = *l_p;
+        myM[value].second = x[2];
+        ++l_p;
+      }
+      un_visited.erase(l_p2, h_p2);
     }
 
-    // merge them O(myM)
-
+    // p1<=p2
     for (auto x : myM)
     {
+      if (x.second.second == -1)
+        continue;
+      // first element
       if (ans.size() == 0)
       {
-        ans.push_back(vector<int>({x.first, x.first + 1, x.second}));
+        ans.push_back({x.first, x.second.first, x.second.second});
         continue;
       }
       // else
-      if (x.second == ans.back().back())
-      {
-        ans.back()[1] = x.first + 1;
-        continue;
-      }
-      // else, have a new height
-      ans.push_back(vector<int>({x.first, x.first + 1, x.second}));
+      if (x.second.second == ans.back().back())
+        ans.back()[1] = x.second.first;
+      else
+        ans.push_back({x.first, x.second.first, x.second.second});
     }
 
     return ans;
